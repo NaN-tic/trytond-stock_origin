b5d76418f35666dadf43bbabab24d1b20b2c0110
resteve <resteve@zikzakmedia.com>
2012-07-23 13:59:02 +0200

    Fix locale ca_ES

--- __tryton__.py
+++ __tryton__.py
@@ -3,14 +3,14 @@
 #the full copyright notices and license terms.
 {
     'name': 'Stock Origin',
-    'name_ca_ES': 'Orígen estoc',
+    'name_ca_ES': 'Origen estoc',
     'name_es_ES': 'Origen stock',
     'version': '2.4.0',
     'author': 'Zikzakmedia',
     'email': 'zikzak@zikzakmedia.com',
     'website': 'http://www.zikzakmedia.com/',
     'description': '''Add origin of shipment: sale, TPV...''',
-    'description_ca_ES': '''Afegeix l'orígen del albarà: venda, TPV...''',
+    'description_ca_ES': '''Afegeix l'origen del albarà: venda, TPV...''',
     'description_es_ES': '''Añade el origen del albarán: venta, TPV...''',
     'depends': [
         'ir',
