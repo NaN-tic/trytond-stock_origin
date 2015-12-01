c4d792bfaae1b1f344743a2002776486a06d02ea
resteve <resteve@zikzakmedia.com>
2015-12-01 11:25:24 +0100

    Tests

--- tests/test_stock_origin.py
+++ tests/test_stock_origin.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part stock_origin module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the stock_origin module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockOriginTestCase(unittest.TestCase):
+class StockOriginTestCase(ModuleTestCase):
     'Test Stock Origin module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_origin')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_origin')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_origin'
 
 
 def suite():
